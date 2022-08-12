3b10ee5bd27b63645bbc3fc0d4774ce92f42176c
Esteban Carignani <estebancarignani99@gmail.com>
2022-08-12 20:27:06 -0300

    Config

--- Project/ParkIT/src/main/java/servlet/AdministrarTipoVehiculoController.java
+++ Project/ParkIT/src/main/java/servlet/AdministrarTipoVehiculoController.java
@@ -12,46 +12,35 @@ import javax.servlet.http.HttpServletResponse;
 import entities.Vehiculos.TipoVehiculo;
 import logic.Vehiculos.AdministrarTipoVehiculoLogic;
 
-/**
- * Servlet implementation class AdministrarTipoVehiculo
- */
 @WebServlet("/AdministrarTipoVehiculo")
 public class AdministrarTipoVehiculoController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
     
 	private AdministrarTipoVehiculoLogic Logic;
 	
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
     public AdministrarTipoVehiculoController() {
         super();
-        // TODO Auto-generated constructor stub
         
         this.Logic = AdministrarTipoVehiculoLogic.getIntancia();
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		LinkedList<TipoVehiculo> tiposVehiculo = this.Logic.getAll();
-		request.setAttribute("ListaTiposVehiculo", tiposVehiculo);
 		
+		LinkedList<TipoVehiculo> tiposVehiculo = new LinkedList<>();
+		tiposVehiculo = this.Logic.getAll();
+		
+		request.setAttribute("ListaTiposVehiculo", tiposVehiculo);
 		request.getRequestDispatcher("AdministrarTipoVehiculo.jsp").include(request, response);
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		
 		TipoVehiculo tipoVehiculo = new TipoVehiculo();
 		tipoVehiculo.setDescripcion(request.getParameter("Descripcion"));
 		
 		this.Logic.add(tipoVehiculo);
 		
+		this.doGet(request, response);
 	}
 
 }
